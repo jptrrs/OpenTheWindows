1c053261bf4523ca0fb4f4c3c7668777c144b834
João Pedro Torres <jaopedro05@outlook.com>
2021-04-20 00:35:23 -0300

    cleanup

---
+++ Source/HarmonyPatches/CompFlickable_PostExposeData.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace OpenTheWindows
+{
+    //Initialize Window components on spawn.
+    [HarmonyPatch(typeof(CompFlickable), nameof(CompFlickable.PostExposeData))]
+    public static class CompFlickable_PostExposeData
+    {
+        public static bool Prefix(CompFlickable __instance, bool ___switchOnInt, bool ___wantSwitchOn)
+        {
+            if (!(__instance is CompWindow compWindow)) return true;
+            if (Scribe.mode == LoadSaveMode.PostLoadInit) compWindow.SetupState();
+            return false;
+        }
+    }
+}
