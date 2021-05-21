aa114dabb3dac33c595374010d2c7f88f3a9b4d8
João Pedro Torres <jaopedro05@outlook.com>
2021-05-21 16:46:01 -0300

    Fixed error drawing ghost area when selecting a window blueprint.

--- Source/PlaceWorker_Window.cs
+++ Source/PlaceWorker_Window.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Verse;
 
@@ -8,18 +10,20 @@ namespace OpenTheWindows
     {
         public override void DrawGhost(ThingDef def, IntVec3 center, Rot4 rot, Color ghostCol, Thing thing = null)
         {
-            if (thing == null)
+            List<IntVec3> field;
+            if (thing == null || thing is Blueprint)
             {
                 Map currentMap = Find.CurrentMap;
                 IntVec3 start = WindowUtility.FindEnd(center, rot, def.size, false);
                 IntVec3 end = WindowUtility.FindEnd(center, rot, def.size, true);
-                GenDraw.DrawFieldEdges(WindowUtility.GetWindowObfuscation(def.size, center, rot, currentMap, start, end).ToList());
+                field = WindowUtility.GetWindowObfuscation(def.size, center, rot, currentMap, start, end).ToList();
             }
             else
             {
                 var window = thing as Building_Window;
-                GenDraw.DrawFieldEdges(window.EffectArea);
+                field = window.EffectArea;
             }
+            GenDraw.DrawFieldEdges(field);
         }
     }
 }
